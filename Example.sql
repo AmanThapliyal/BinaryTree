cd313ff57e809686c6186855957dc666385d2f45
AmanThapliyal <amanthapliyal19@gmail.com>
2024-06-20 22:45:22 +0530

    Example.sql

---
+++ Example.sql
@@ -0,0 +1,17 @@
+Enter a number for the current node: 50
+Do you want to insert another node? (y/n): y
+Enter a number for the current node: 30
+Do you want to insert another node? (y/n): y
+Enter a number for the current node: 70
+Do you want to insert another node? (y/n): n
+Traversal of the binary tree
+Inorder traversal
+30 50 70 
+Preorder traversal
+50 30 70 
+Postorder traversal
+30 70 50 
+Enter the element to delete: 30
+Binary tree after deletion:
+Inorder traversal
+50 70
